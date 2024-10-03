1211f5668e570f6566a57ccbc481d2d8c044fabf
nishal <nishalk9399@gmail.com>
2024-10-03 10:27:46 +0530

    updated shared lib parameters

--- vars/pipelineDecission.groovy
+++ vars/pipelineDecission.groovy
@@ -1,5 +1,22 @@
 #!groovy
 
 def decidepipeline(Map configMap){
+    application = configMap.get("application")
+
+    //here we are getting nodeJSVM
+
+    switch(application) {
+        case 'nodeJSVM':
+            echo "Application is nodeJS and VM based"
+
+            //nodeJSVMCI(configMap)
+            break
+        case 'JavaVM':
+            javaVMCI(configMap)
+            break
+        default: 
+            error "un recognised application"
+            break
+    }
     echo "I need to take the decission for your map"
 }
